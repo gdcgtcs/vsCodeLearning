99391e60771213bc800970c974acfd758719023c
Guillermo Chang <guillermo.chang.tcs@gmail.com>
2025-08-05 16:58:42 -0600

    SOLID Principles

--- learning/src/main/java/com/examples/solidprinciples/SingleResponsabilityPrinciple.java
+++ learning/src/main/java/com/examples/solidprinciples/SingleResponsabilityPrinciple.java
@@ -9,4 +9,55 @@ public class SingleResponsabilityPrinciple {
      * within the software system.
      */
 
+    // Class for baking bread
+    static class BreadBaker {
+        public void bakeBread() {
+            System.out.println("Baking high-quality bread...");
+        }
+    }
+
+    // Class for managing inventory
+    static class InventoryManager {
+        public void manageInventory() {
+            System.out.println("Managing inventory...");
+        }
+    }
+
+    // Class for ordering cupplies
+    static class SupplyOrder {
+        public void orderSuplies() {
+            System.out.println("Ordering supplies...");
+        }
+    }
+
+    // Class for serving customers
+    static class CustomerService {
+        public void serveCustomer() {
+            System.out.println("Serving customers...");
+        }
+    }
+
+    // Class for cleaning the bakery
+    static class BakeryCleaner {
+        public void cleanBakery() {
+            System.out.println("Claning the bakery...");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Each class focuses on its specific responsability
+        BreadBaker baker = new BreadBaker();
+        InventoryManager inventoryManager = new InventoryManager();
+        SupplyOrder supplyOrder = new SupplyOrder();
+        CustomerService customerService = new CustomerService();
+        BakeryCleaner cleaner = new BakeryCleaner();
+
+        baker.bakeBread();
+        inventoryManager.manageInventory();
+        supplyOrder.orderSuplies();
+        customerService.serveCustomer();
+        cleaner.cleanBakery();
+
+    }
+
 }
