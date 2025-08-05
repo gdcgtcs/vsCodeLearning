236227129cae9bad710cba3b76ec6ad62db99dc1
Guillermo Chang <guillermo.chang.tcs@gmail.com>
2025-08-05 16:23:16 -0600

    Threads Examples

---
+++ learning/src/main/java/com/examples/solidprinciples/SingleResponsabilityPrinciple.java
@@ -0,0 +1,12 @@
+package com.examples.solidprinciples;
+
+public class SingleResponsabilityPrinciple {
+
+    /*
+     * This principle states that "A class should have only one reason to change"
+     * which means every class should have a single responsibility or single job or
+     * single purpose. In other words, a class should have only one job or purpose
+     * within the software system.
+     */
+
+}
